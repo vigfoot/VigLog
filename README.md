cac1ea1f57339747dcdd1a68737efb42a4fb627d
HyeonSeok Ko <bigfoot7774@gmail.com>
2023-11-13 13:34:57 +0900

    고민

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # viglog
 
 - java version : JDK 5(1.5)
+
+## TODO
+- log 라이브러리를 만들고 싶다.
+- 뭐부터 해야할까.
---
+++ src/com/vigfoot/VigLog.java
@@ -0,0 +1,11 @@
+package com.vigfoot;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface VigLog {
+
+
+
+}
